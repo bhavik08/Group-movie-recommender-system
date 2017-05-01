d34dbcf54b34efb7883a50928fee8457f0d5638c
jshobhitj <jshobhitj.7@gmail.com>
2017-05-01 15:29:25 -0500

    Refractoring code.

--- Aggregators.py
+++ Aggregators.py
@@ -1,12 +1,10 @@
-
-import math
 import numpy as np
+
 
 class Aggregators:
     def __init__(self):
         pass
-    
-    #pass ratings or factors as input
+
     @staticmethod
     def average(arr):
         return np.average(arr, axis = 0, weights = None)
@@ -19,20 +17,3 @@ class Aggregators:
     @staticmethod
     def weighted_average(arr, weights):
         return np.average(arr, axis = 0, weights = weights)
-    
-    @staticmethod
-    def mode(arr):
-        pass
-        
-    @staticmethod
-    def median(arr):
-        pass
-    
-    @staticmethod
-    def least_misery(self, arr):
-        pass
-    
-    @staticmethod
-    def most_pleasure(self, arr):
-        pass
-    
